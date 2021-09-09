c925ef398a4bddf3fba3bfd3be6ab5e79f302d37
Wei Lyu <lvwei1001@live.com>
2021-09-09 23:55:59 +0900

    display image

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ void main() {
           backgroundColor: Colors.blueGrey[900],
         ),
         backgroundColor: Colors.blueGrey[100],
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'),
+          ),
+        ),
       ),
     ),
   );
