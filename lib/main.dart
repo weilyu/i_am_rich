05c89a92ee870b68ee940f10ef526dd136cba73d
Wei Lyu <lvwei1001@live.com>
2021-09-09 23:38:08 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey[100],
+      ),
+    ),
+  );
+}
