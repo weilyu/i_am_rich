f49feb492b86d9e5b5e7468f790429d29e1a3ef7
Wei Lyu <lvwei1001@live.com>
2021-09-13 20:47:06 +0900

    use asset image

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,7 @@ void main() {
         backgroundColor: Colors.blueGrey[100],
         body: const Center(
           child: Image(
-            image: NetworkImage(
-                'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
